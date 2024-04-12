dedaa662c2819527bbb9098be6b7f27673030f60
JoseMTrias <jmgarciatrias@gmail.com>
2024-04-12 12:15:55 +0200

    setting winner name

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,12 +1,21 @@
 import { useState } from 'react';
 
-export default function Player({ initialName, symbol, isActive }) {
+export default function Player({
+  initialName,
+  symbol,
+  isActive,
+  onChangeName,
+}) {
   const [isEditing, setIsEditing] = useState(false);
   const [playerName, setPlayerName] = useState(initialName);
 
   function handleEditClick() {
     //setIsEditing(!isEditing) not ideal
     setIsEditing((editing) => !editing); // best practice
+
+    if (isEditing) {
+      onChangeName(symbol, playerName);
+    }
   }
 
   function handleChange(event) {
